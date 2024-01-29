27b7f69fa2ee54c8309e309abddf2997a08d091a
=oaksas <=>
2024-01-29 08:41:10 +0300

    Add applicaiton model

--- apps/job/models.py
+++ apps/job/models.py
@@ -15,3 +15,20 @@ class Job(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Application(models.Model):
+    job = models.ForeignKey(
+        Job, related_name='applications', on_delete=models.CASCADE)
+    content = models.TextField(blank=True, null=True)
+    experience = models.TextField(blank=True, null=True)
+    created_by = models.ForeignKey(
+        User, related_name='applications', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ['job', 'created_by']
+
+    def __str__(self):
+        return self.job.title + ' - ' + self.created_by.username
