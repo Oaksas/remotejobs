1964f7b1721e3da2126059e40c18fee5ccfad4cd
=oaksas <=>
2024-01-30 10:55:54 +0300

    Add dashboard view for a job

--- apps/userprofile/urls.py
+++ apps/userprofile/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import dashboard, view_application
+from .views import dashboard, view_application, view_dashboard_job
 
 urlpatterns = [
     path('', dashboard, name="dashboard"),
     path('application/<int:application_id>',
-         view_application, name="view_application")
+         view_application, name="view_application"),
+    path('job/<int:job_id>', view_dashboard_job, name="view_dashboard_job"),
+
 ]
--- apps/userprofile/views.py
+++ apps/userprofile/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from apps.job.models import Application
+from apps.job.models import Application, Job
 
 
 @login_required
@@ -13,3 +13,9 @@ def view_application(request, application_id):
     application = get_object_or_404(
         Application, pk=application_id, created_by=request.user)
     return render(request, 'userprofile/view_application.html', {'application': application})
+
+
+@login_required
+def view_dashboard_job(request, job_id):
+    job = get_object_or_404(Job, pk=job_id, created_by=request.user)
+    return render(request, 'userprofile/view_dashboard_job.html', {'job': job})
